f5633827e8bdf2710e3966cb1719a68069a91adf
Riz-baig <baigrizmirza@gmial.com>
2025-09-23 17:48:09 +0200

    transoformar numero decimal con cualquier otra base

---
+++ UT2_02_Bucles.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+  $num="48";
+  $base="2";
+  echo "Numero: ". $num. "<br>";
+  echo "Base: ". $base. "<br>"; 
+  
+  $resultado = "";
+
+  while ($num > 0)
+    {
+        $residuo = $num % $base;
+        $resultado = $residuo.$resultado;
+        $num = floor($num / $base);
+    }
+
+    echo "Resultado: ". $resultado;
+?>
+    
+</body>
+</html>
