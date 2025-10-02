0a5bdb7c4b10d5579a53329461f2d4cc7f1d01f2
Riz-baig <baigrizmirza@gmial.com>
2025-10-02 12:35:35 +0200

    Calculadora

---
+++ UT3_01_Formularios/calculadora.php
@@ -0,0 +1,32 @@
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $op1 = floatval($_POST["operando1"]);
+    $op2 = floatval($_POST["operando2"]);
+    $operacion = $_POST["operacion"];
+    $resultado = "";
+
+    switch ($operacion) {
+        case "suma":
+            $resultado = $op1 + $op2;
+            break;
+        case "resta":
+            $resultado = $op1 - $op2;
+            break;
+        case "producto":
+            $resultado = $op1 * $op2;
+            break;
+        case "division":
+            if ($op2 != 0) {
+                $resultado = $op1 / $op2;
+            } else {
+                $resultado = "Error: división entre 0";
+            }
+            break;
+        default:
+            $resultado = "Operación no válida";
+    }
+
+    echo "<h1>Resultado de la operación: $resultado</h1>";
+}
+?>
